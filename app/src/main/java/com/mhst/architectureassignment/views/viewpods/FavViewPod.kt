73558f8253f8bb9538eb743f0ade0de321d815bf
moehtet <laxus3331@gmail.com>
2020-02-07 01:06:06 +0630

    fav viewpod added

---
+++ app/src/main/java/com/mhst/architectureassignment/views/viewpods/FavViewPod.kt
@@ -0,0 +1,14 @@
+package com.mhst.architectureassignment.views.viewpods
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class FavViewPod @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+    }
+
+}
